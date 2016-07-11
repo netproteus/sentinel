179b6255bda56e6ed35605832914b7f2d3f3ce89
Kieren Beckett <mail@kierenb.net>
2016-07-11 14:13:47 +0100

    Handle missing dynamo keys

--- sentinel/state_plugins/dynamodb.py
+++ sentinel/state_plugins/dynamodb.py
@@ -7,7 +7,12 @@ def get_metadata(config):
     session = boto3.session.Session(aws_access_key_id=config['aws_access_key_id'], aws_secret_access_key=config['aws_secret_access_key'], region_name=config['aws_region'])
     dynamodb = session.resource('dynamodb')
     table = dynamodb.Table(config['table'])
-    item = table.get_item(Key={'name': '_metadata'})['Item']
+    item = table.get_item(Key={'name': '_metadata'})
+
+    if 'Item' not in item:
+        return {}
+
+    item = item['Item']
     item.pop('name')
     for k, v in item.items():
         if isinstance(v, Decimal):
@@ -27,7 +32,14 @@ def get_alert_data(config, name):
     session = boto3.session.Session(aws_access_key_id=config['aws_access_key_id'], aws_secret_access_key=config['aws_secret_access_key'], region_name=config['aws_region'])
     dynamodb = session.resource('dynamodb')
     table = dynamodb.Table(config['table'])
-    item = table.get_item(Key={'name': name})['Item']
+    item = table.get_item(Key={'name': name})
+
+    if 'Item' not in item:
+        return {
+            'keys': {}
+        }
+
+    item = item['Item']
     item.pop('name')
     for k, v in item.items():
         if isinstance(v, Decimal):
--- sentinel/storage_utils.py
+++ sentinel/storage_utils.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 
 STATE_PATH = '/state/'
